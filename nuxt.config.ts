e30b16ea2fbd5a571acd33a333c9c7fa3a55c52f
y0sh1ma <138575434+y0sh1ma@users.noreply.github.com>
2024-04-11 09:30:30 +0900

    Update nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,7 +4,7 @@ export default defineNuxtConfig({
   // ssr: true,
   runtimeConfig: {
     public: {
-      kurocoApiDomain: 'https://diverta-oshima-corp.g.kuroco.app',
+      kurocoApiDomain: 'https://a_diverta-oshima-corp.g.kuroco.app',
     },
   },
   app: {
